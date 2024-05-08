5469afc03ef225290180d7982889aa7987af082a
Stew <65829845+VicStew@users.noreply.github.com>
2024-05-08 18:00:17 +0300

    logging improvements

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
-use esp_println::println;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
 
+    // printing to serial console
+    esp_idf_svc::log::EspLogger::initialize_default();
+    
     let peripherals = Peripherals::take().unwrap();
     let mut led = PinDriver::output(peripherals.pins.gpio4)?;
 
-    println!("Hello world!");
+    log::info!("Hello world!");
     loop {
         led.set_high()?;
         // we are sleeping here to make sure the watchdog isn't triggered
-        FreeRtos::delay_ms(100);
-        println!("LED ON");
+        FreeRtos::delay_ms(1000);
+        log::info!("LED ON");
 
         led.set_low()?;
-        FreeRtos::delay_ms(100);
-        println!("LED OFF");
+        FreeRtos::delay_ms(1000);
+        log::info!("LED OFF");
     }
 }
